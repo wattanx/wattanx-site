a5841099acbd5f4b22bac668f9dba4828757e79e
wattanx <wattan.dev@gmail.com>
2023-01-06 12:40:25 +0900

    feat: blog途中

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,9 +1,6 @@
-export const SiteHeader: React.FC<{ borderBottomColor?: string }> = ({
-  borderBottomColor,
-}) => {
-  const border = `border-[1px] border-t-0 border-x-0 ${borderBottomColor}`;
+export const SiteHeader: React.FC = () => {
   return (
-    <header className={`bg-gray-900 sticky py-4 px-0 top-0 z-[99] ${border}`}>
+    <header className={`bg-gray-900 sticky py-4 px-0 top-0 z-[99]`}>
       <div className="my-0 mx-auto max-w-[850px] py-0 px-6">
         <div className="flex flex-row items-center font-bold justify-between">
           <div>
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -61,3 +61,17 @@ const months = [
   "Dec.",
 ];
 export const getStringOfMonth = (month: number) => months[month - 1];
+
+export function formatDate(date: Date) {
+  const parsed = Object.fromEntries(
+    new Intl.DateTimeFormat("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+      .formatToParts(date)
+      .map(({ type, value }) => [type, value])
+  );
+
+  return `${parsed.year}${parsed.literal}${parsed.month}${parsed.literal}${parsed.day}`;
+}
