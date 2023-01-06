e75715779da183b5a20370596907e013fd3e67d7
wattanx <wattan.dev@gmail.com>
2023-01-06 15:26:14 +0900

    fix: astroファイルに変更

--- src/components/Center.tsx
+++
@@ -1,5 +0,0 @@
-import type React from "react";
-
-export const Center: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => <div className="flex items-center justify-center">{children}</div>;
--- src/components/Stack.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react";
-
-export const Stack: React.FC<{
-  children: React.ReactNode;
-  spacing: number;
-}> = ({ children, spacing = 4 }) => {
-  const space = `space-y-${spacing}`;
-  return <div className={`flex ${space}`}>{children}</div>;
-};
