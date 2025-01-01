2c07cdf01a66ccdf425b80bfc516d7806d534a04
Ryota Watanabe <43837308+wattanx@users.noreply.github.com>
2025-01-01 17:37:16 +0900

    feat: light mode (#61)
    * feat: light mode
    
    * fix: fix style
    
    * fix: fix style

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -2,5 +2,8 @@ import { getCollection } from "astro:content";
 
 export const getBlogs = async () => {
   const posts = await getCollection("blog");
-  return posts;
+  // date の降順に並び替え
+  return posts.sort(
+    (a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
+  );
 };
