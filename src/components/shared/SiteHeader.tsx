b6bf7c2a7c420b1b6973c595085a157fc0002031
wattanx <wattan.dev@gmail.com>
2025-01-01 17:14:59 +0900

    fix: fix style

--- src/components/shared/SiteHeader.tsx
+++ src/components/shared/SiteHeader.tsx
@@ -1,8 +1,6 @@
 export const SiteHeader: React.FC = () => {
   return (
-    <header
-      className={`dark:bg-gray-900 bg-[#fdfdfc] sticky py-4 px-0 top-0 z-[99]`}
-    >
+    <header className={`dark:bg-gray-900 bg-[#fdfdfc] py-4 px-0`}>
       <div className="my-0 mx-auto max-w-[850px] py-0 px-6">
         <div className="flex flex-row items-center font-bold justify-between">
           <div>
